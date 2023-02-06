7e8d03514f77967ca325cfd8cdefdcf64b921563
sarah <sarahdavies186@gmail.com>
2023-02-06 16:43:22 +0000

    added interactive menu

--- directory.rb
+++ directory.rb
@@ -17,6 +17,28 @@ def input_students
   students
 end
 
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit #this will exit the programme
+    else
+      puts "I don't know what you mean, try again"
+    end
+  end
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -34,7 +56,4 @@ end
 
 #nothing happens until we call the methods
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
